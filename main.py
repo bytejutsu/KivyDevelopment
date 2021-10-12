de565086124b4ea9a3fd2af2605d00ea36ff7da5
Dhia Nabli <dhianabli@outlook.com>
2021-10-12 03:45:06 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+# interface
+class Interface(Widget):
+    pass
+
+
+# app creation
+class TestApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    TestApp().run()
